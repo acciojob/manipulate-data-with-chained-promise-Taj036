5ac63fc6b41307a05f2ff5f5f5221671697e6cd1
Taj036 <121137573+Taj036@users.noreply.github.com>
2024-01-19 22:51:21 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 //your JS code here. If required.
+function processArray(arr) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(arr);
+    }, 3000);
+  });
+}
+function filterOddNumbers(arr) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const oddNumbers = arr.filter((number) => number % 2 !== 0);
+      resolve(oddNumbers);
+    }, 1000);
+  });
+}
+
+function multiplyEvenNumbers(arr) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const evenNumbersMultiplied = arr.map((number) => (number % 2 === 0 ? number * 2 : number));
+      resolve(evenNumbersMultiplied);
+    }, 2000);
+  });
+}
+const inputArray = [1, 2, 3, 4];
+
+processArray(inputArray)
+  .then((result) => {
+    document.getElementById('output').innerText = ` ${result}`;
+    return filterOddNumbers(result);
+  })
+
+ .then((result) => {
+    document.getElementById('output').innerText += `\n ${result}`;
+    return multiplyEvenNumbers(result);
+  })
+  .then((result) => {
+    document.getElementById('output').innerText += `\n ${result}`;
+  })
+  .catch((error) => {
+
+    console.error(error);
+  });
