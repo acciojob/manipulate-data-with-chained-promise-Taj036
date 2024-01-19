66ec6df49a144e821c75b7cb23f0dbaf9308b952
Taj036 <121137573+Taj036@users.noreply.github.com>
2024-01-19 23:00:14 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,44 +1,37 @@
 //your JS code here. If required.
-function processArray(arr) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(arr);
-    }, 3000);
-  });
-}
-function filterOddNumbers(arr) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const oddNumbers = arr.filter((number) => number % 2 !== 0);
-      resolve(oddNumbers);
-    }, 1000);
-  });
-}
-
-function multiplyEvenNumbers(arr) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const evenNumbersMultiplied = arr.map((number) => (number % 2 === 0 ? number * 2 : number));
-      resolve(evenNumbersMultiplied);
-    }, 2000);
-  });
-}
-const inputArray = [1, 2, 3, 4];
-
-processArray(inputArray)
-  .then((result) => {
-    document.getElementById('output').innerText = ` ${result}`;
-    return filterOddNumbers(result);
-  })
-
- .then((result) => {
-    document.getElementById('output').innerText += `\n ${result}`;
-    return multiplyEvenNumbers(result);
-  })
-  .then((result) => {
-    document.getElementById('output').innerText += `\n ${result}`;
-  })
-  .catch((error) => {
-
-    console.error(error);
-  });
+let arr = [1 , 2 , 3 , 4 , 5, 6];
+let promise1 = new Promise((resolve , reject)=>{
+  setTimeout(()=>{
+    const evenNumbers = arr.filter((number)=>number % 2 !==0);
+    if(arr.length > 0){
+     resolve(evenNumbers);
+    }else{
+     reject('array is empty')
+    }
+  },3000)
+  
+})
+let promise2 = new Promise((resolve , reject)=>{
+  setTimeout(()=>{
+    const multi = [] ;
+    if(arr.length > 0){
+      arr.forEach((e)=>{
+        if(e % 2===0){
+          multi.push(e * 2);
+        }
+      })
+     resolve(multi);
+    }else{
+     reject('array is empty')
+    }
+  },2000)
+  
+})
+promise1.then((data1) => {
+  document.getElementById('output').innerText = data1;
+  return promise2;
+}).then((data2) => {
+  document.getElementById('output').innerText += '\n' + data2;
+}).catch((error) => {
+  console.error(error);
+});
